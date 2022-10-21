971e53e2e3cee6c49d47cb82fe4dae40f1d79de3
Bruno <br.ananias@gmail.com>
2022-10-20 21:00:54 -0300

    Configurando nodemon e git ignore

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Página de Livros',
+    '/autores': 'Página de Autores',
+    '/editoras': 'Editoras',
+    '\sobre': 'Infos sobre o Projeto'
+}
 const server = http.createServer((req, res)=>{
     res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end("Curso Node - Alura")
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, ()=>{
